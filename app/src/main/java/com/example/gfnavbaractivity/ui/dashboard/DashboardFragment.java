0790624cf420f1043700d6cf111d489646d4ce09
esket <esket@WINDOWS-OMU96BF>
2019-10-30 10:29:04 -0400

    First android studio work session - Evan

--- app/src/main/java/com/example/gfnavbaractivity/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/gfnavbaractivity/ui/dashboard/DashboardFragment.java
@@ -22,8 +22,8 @@ public class DashboardFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
+        View root = inflater.inflate(R.layout.profile, container, false);
+        final TextView textView = root.findViewById(R.id.text_profile);
         dashboardViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
