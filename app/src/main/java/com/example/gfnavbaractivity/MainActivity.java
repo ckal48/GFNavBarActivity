2c25e30f19b116c9c2aa634a68cfa2d44a5410b3
William Kent <wjk011@gmail.com>
2019-11-13 10:50:49 -0500

    Revert back to c42b1e7

--- app/src/main/java/com/example/gfnavbaractivity/MainActivity.java
+++ app/src/main/java/com/example/gfnavbaractivity/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.gfnavbaractivity;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -11,33 +8,13 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
-
-    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-
-        listView = findViewById(R.id.schedule_listView);
-
-        ArrayList<String> classes = new ArrayList<>();
-
-        classes.add("CSI 2400");
-        classes.add("CSI 2999");
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                classes
-        );
-
-        listView.setAdapter(adapter);
-
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
