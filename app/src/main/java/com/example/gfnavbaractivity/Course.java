6e57786cf6d8590f3e813cf0476cf19c4f796259
Charles Kalajian <cfkalajian34@gmail.com>
2019-12-03 00:20:07 -0500

    Session Functionality for Create Session Button

---
+++ app/src/main/java/com/example/gfnavbaractivity/Course.java
@@ -0,0 +1,29 @@
+package com.example.gfnavbaractivity;
+
+public class Course {
+    String title;
+    Boolean monday;
+    Boolean tuesday;
+    Boolean wednesday;
+    Boolean thursday;
+    Boolean friday;
+    Boolean saturday;
+    Boolean sunday;
+    String startTime; //not sure what
+    String endTime;   //values time gives
+                      //use String for now...
+
+    public Course(String courseName, Boolean m, Boolean t, Boolean w, Boolean r,
+                  Boolean f, Boolean sA, Boolean sU, String sTime, String eTime){
+        title = courseName;
+        monday = m;
+        tuesday = t;
+        wednesday = w;
+        thursday = r;
+        friday = f;
+        saturday = sA;
+        sunday = sU;
+        startTime =sTime;
+        endTime = eTime;
+    }
+}
--- app/src/main/java/com/example/gfnavbaractivity/MainActivity.java
+++ app/src/main/java/com/example/gfnavbaractivity/MainActivity.java
@@ -1,24 +1,70 @@
 package com.example.gfnavbaractivity;
 
+import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button createButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        //code for create button in Session layout
+        createButton = (Button) findViewById(R.id.createButton);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreateButtonActivity();
+            }
+        });
+
+
+
+
+        final BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+
+
+        /*NavController navController = new NavController((this.getApplicationContext()));
+        //not sure if navcontroller is properly instantiated
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller,
+                                             @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                if(destination.getId() == R.id.action_navigation_schedule_to_navigation_addClass) {
+                    bottomNav.setVisibility(View.GONE);
+                }
+                else {
+                    bottomNav.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+         */
+        //makes the bottomnavbar disappear when on add class screen
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new StartFragment()).commit();
@@ -26,6 +72,11 @@ public class MainActivity extends AppCompatActivity {
         Toast startmessage = Toast.makeText(getApplicationContext(), "Welcome to GrizzFinder! Click to continue",Toast.LENGTH_LONG);
         startmessage.setGravity(Gravity.BOTTOM,0,250);
         startmessage.show();
+    }
+
+    public void openCreateButtonActivity() {
+        Intent intent = new Intent(this, CreateButtonActitivity.class);
+        startActivity(intent);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
@@ -57,6 +108,8 @@ public class MainActivity extends AppCompatActivity {
 
                 }
             };
+
+
 
 }
 
