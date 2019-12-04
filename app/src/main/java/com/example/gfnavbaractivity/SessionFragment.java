198a373a2bdfb756fb22a0e8c62e7224860f5a17
esket <esket@WINDOWS-OMU96BF>
2019-12-04 11:15:23 -0500

    Helped Charles with his stuff

--- app/src/main/java/com/example/gfnavbaractivity/SessionFragment.java
+++ app/src/main/java/com/example/gfnavbaractivity/SessionFragment.java
@@ -4,16 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 public class SessionFragment extends Fragment {
+
+    Button sessionScreen;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.session, container, false);
+        View view = inflater.inflate(R.layout.session, container, false);
+
+        sessionScreen= view.findViewById(R.id.createButton);
+        sessionScreen.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_navigation_session_to_navigation_activity_create_button_activity, null));
+
+        return view;
     }
 }
