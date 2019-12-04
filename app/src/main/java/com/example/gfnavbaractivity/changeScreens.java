702c42550fba535393b503efc15430261b100049
bridget <babmi@DESKTOP-BHC843T>
2019-12-03 21:46:36 -0500

    testng stuff out nt sure if it works

---
+++ app/src/main/java/com/example/gfnavbaractivity/changeScreens.java
@@ -0,0 +1,14 @@
+package com.example.gfnavbaractivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class changeScreens extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_screens);
+    }
+}
