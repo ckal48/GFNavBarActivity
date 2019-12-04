0815b160a3714763329f1c77957a2e6049412b21
bridget <babmi@DESKTOP-BHC843T>
2019-12-04 02:43:19 -0500

    finishing up switching between layouts and putting the input of the editText from one to the other

--- app/src/main/java/com/example/gfnavbaractivity/ProfileFragment.java
+++ app/src/main/java/com/example/gfnavbaractivity/ProfileFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,12 +14,28 @@ import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
     private Button button;
+    private TextView name, major, motto, description;
+    private String st1, st2, st3, st4;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.profile, container, false);
 
         button = (Button) getView().findViewById(R.id.button);
+        name = (TextView) getView().findViewById(R.id.name);
+        major = (TextView) getView().findViewById(R.id.major);
+        motto = (TextView) getView().findViewById(R.id.motto);
+        description = (TextView) getView().findViewById(R.id.description);
+        st1 = getIntent().getExtras().getString("your_name");
+        st2 = getIntent().getExtras().getString("your_major");
+        st3 = getIntent().getExtras().getString("your_words_to_live_by");
+        st4 = getIntent().getExtras().getString("fill_in");
+        name.setText(st1);
+        major.setText(st2);
+        motto.setText(st3);
+        description.setText(st4);
+
+
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
--- app/src/main/java/com/example/gfnavbaractivity/profile2.java
+++ app/src/main/java/com/example/gfnavbaractivity/profile2.java
@@ -12,8 +12,10 @@ import android.widget.EditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class profile2 extends AppCompatActivity {
-    private Button mButton;
-    private EditText text1, text2, text3, text4;
+    Button mButton;
+    EditText text1, text2, text3, text4;
+    String st1, st2, st3,st4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,10 +30,23 @@ public class profile2 extends AppCompatActivity {
         mButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Log.v("EditText", text1.getText().toString());
-                Log.v("EditText", text2.getText().toString());
-                Log.v("EditText", text3.getText().toString());
-                Log.v("EditText", text4.getText().toString());
+                Intent one = new Intent(profile2.this, ProfileFragment.class);
+                Intent two = new Intent(profile2.this, ProfileFragment.class);
+                Intent three = new Intent(profile2.this, ProfileFragment.class);
+                Intent four = new Intent(profile2.this, ProfileFragment.class);
+                st1 = text1.getText().toString();
+                st2 = text2.getText().toString();
+                st3 = text3.getText().toString();
+                st4 = text4.getText().toString();
+                one.putExtra("your_name", st1);
+                two.putExtra("your_major", st2);
+                three.putExtra("your_words_to_live_by", st3);
+                four.putExtra("fill_in", st4);
+                startActivity(one);
+                startActivity(two);
+                startActivity(three);
+                startActivity(four);
+                finish();
             }
         });
     }
